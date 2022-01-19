58d18ca430f7a3470ec967f68a977d200e295b63
Tsiry Andrian <tsiryandrian@gmail.com>
2022-01-19 14:39:46 +0300

    fin nosetests

--- wsgi.py
+++ wsgi.py
@@ -11,10 +11,11 @@ def hello():
     #return "Hello World!"
     return c
 
-@app.route("/api/v1/produits")
-def produits():
+@app.route("/api/v1/products")
+def products():
     PRODUCTS = {
-        1: { 'id': 1, 'name': 'Skello' },
-        2: { 'id': 2, 'name': 'Socialive.tv' },
-    }
+    1: { 'id': 1, 'name': 'Skello' },
+    2: { 'id': 2, 'name': 'Socialive.tv'},
+    3: { 'id': 3, 'name': 'Le Wagon'},
+}
     return PRODUCTS
